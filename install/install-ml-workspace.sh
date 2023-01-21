ed4ad7850172a248c8fe93c1f04acc12d8dadd8d
laurent prud'hon <laurent.prudhon@hotmail.fr>
2023-01-22 00:20:47 +0100

    Extract install-ubuntu-packages.sh

--- install/install-ml-workspace.sh
+++ install/install-ml-workspace.sh
@@ -1,17 +1,5 @@
-. ~/ml-workspace/install/01-ENV.sh
-. ~/ml-workspace/install/01-init-environment.sh                 #   130 MB ->   130 MB
-. ~/ml-workspace/install/02-init-package-install-build.sh       #   323 MB ->   453 MB
-. ~/ml-workspace/install/03-install-system-tools.sh             #    11 MB ->   465 MB
-. ~/ml-workspace/install/04-install-network-tools.sh            #    88 MB ->   553 MB
-. ~/ml-workspace/install/05-install-terminal-tools.sh           #    56 MB ->   610 MB
-. ~/ml-workspace/install/06-install-database-tools.sh           #    21 MB ->   632 MB
-. ~/ml-workspace/install/07-install-version-control-systems.sh  #    27 MB ->   660 MB
-. ~/ml-workspace/install/08-install-images-graph-tools.sh       #    96 MB ->   756 MB
-. ~/ml-workspace/install/09-install-X11-extensions.sh           #     5 MB ->   762 MB
-. ~/ml-workspace/install/10-install-protobuf-build-tools.sh     #    76 MB ->   838 MB
-. ~/ml-workspace/install/11-install-compression-tools.sh        #     1 MB ->   839 MB
-. ~/ml-workspace/install/12-install-openssh-tools.sh            #     2 MB ->   841 MB
-. ~/ml-workspace/install/13-install-container-entrypoint.sh     #     0 MB ->   841 MB
+. ~/ml-workspace/install/install-ubuntu-packages.sh
+
 . ~/ml-workspace/install/14-ENV.sh
 . ~/ml-workspace/install/14-install-openresty-web-gateway.sh    #    31 MB ->   873 MB
 . ~/ml-workspace/install/15-ENV.sh
@@ -29,8 +17,6 @@
 . ~/ml-workspace/install/22-post-ENV.sh
 . ~/ml-workspace/install/23-ENV.sh
 . ~/ml-workspace/install/23-install-datascience-librairies.sh   #  9344 MB -> 14805 MB
-
-
 
 
 
